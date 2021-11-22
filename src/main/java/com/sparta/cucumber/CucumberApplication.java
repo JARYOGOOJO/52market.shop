770f1f5af7ee962f683c67b26236a696d12416f5
AndrewDongminYoo <ydm2790@gmail.com>
2021-11-22 12:16:00 +0900

    init

---
+++ src/main/java/com/sparta/cucumber/CucumberApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/cucumber/CucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
