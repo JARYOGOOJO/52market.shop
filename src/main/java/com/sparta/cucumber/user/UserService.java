cf72702506b08bb10b0c4340bde0e1437c65ae78
ydm2790 <ydm2790@gmail.com>
2021-11-25 21:19:32 +0900

    test fix

--- src/main/java/com/sparta/cucumber/user/UserService.java
+++ src/main/java/com/sparta/cucumber/user/UserService.java
@@ -1,20 +1,15 @@
 package com.sparta.cucumber.user;
 
-import com.sparta.cucumber.models.Article;
-import com.sparta.cucumber.models.Comment;
 import com.sparta.cucumber.repository.ArticleRepository;
 import com.sparta.cucumber.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    private final CommentRepository commentRepository;
-    private final ArticleRepository articleRepository;
     private final UserRepository userRepository;
 
     @Transactional
--- src/test/java/com/sparta/cucumber/models/ArticleTest.java
+++ src/test/java/com/sparta/cucumber/models/ArticleTest.java
@@ -1,7 +1,6 @@
 package com.sparta.cucumber.models;
 
 import com.sparta.cucumber.repository.ArticleRepository;
-import com.sparta.cucumber.user.Role;
 import com.sparta.cucumber.user.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.sparta.cucumber.user.User;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,21 +33,21 @@ class ArticleTest {
         String title = "타이틀";
         String content = "컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠";
         String image = "https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory4.daumcdn.net%2Ftistory%2F3962924%2Fattach%2F3a33fb3fbfb04050bd15efddb834038f";
-        User user = User
+        User user1 = User
                 .builder()
                 .name("유동민")
                 .email("ydm2790@naver.com")
                 .picture(image)
                 .build();
-        userRepository.save(user);
+        userRepository.save(user1);
         Article article = Article
                 .builder()
                 .title(title)
                 .content(content)
                 .image(image)
-                .user(user)
-                .latitude(37.592252158153975)
-                .longitude(127.08723572001708)
+                .user(user1)
+                .latitude(38.592252158153975)
+                .longitude(128.08723572001708)
                 .build();
         articleRepository.save(article);
 
