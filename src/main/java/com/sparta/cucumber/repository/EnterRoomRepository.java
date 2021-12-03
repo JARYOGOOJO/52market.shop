eb96c618ff6647fff97e34563fde5dbc325df14e
sjh <thdwngus2@naver.com>
2021-12-03 11:23:55 +0900

    feat room enter

---
+++ src/main/java/com/sparta/cucumber/repository/EnterRoomRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.cucumber.repository;
+
+import com.sparta.cucumber.models.EnterRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnterRoomRepository extends JpaRepository<EnterRoom,Long> {
+
+}
