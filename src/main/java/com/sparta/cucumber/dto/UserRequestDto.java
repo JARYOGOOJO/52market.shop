fd0d8a7b04620d916495bdc499377558e42464d7
ydm2790 <ydm2790@gmail.com>
2021-12-07 23:27:06 +0900

    52market.shop-68 Request, Response 간 데이터 교환 형태 점검
    Signed-off-by: ydm2790 <ydm2790@gmail.com>

--- src/main/java/com/sparta/cucumber/dto/UserRequestDto.java
+++ src/main/java/com/sparta/cucumber/dto/UserRequestDto.java
@@ -1,14 +1,19 @@
 package com.sparta.cucumber.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class UserRequestDto {
     private String name;
+    @JsonIgnore(true)
     private String password;
+    @JsonIgnore(true)
     private String phoneNumber;
     private String email;
+    @JsonIgnore(true)
     private Double latitude;
+    @JsonIgnore(true)
     private Double longitude;
     private String picture;
 }
