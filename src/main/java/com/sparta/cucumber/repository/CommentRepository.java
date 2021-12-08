9f5c7aabadbe5eb49d6ad4fcc19545d54264ca09
ydm2790 <ydm2790@gmail.com>
2021-12-08 17:47:59 +0900

    52market.shop-73 EntityGraph 를 이용하여 N+1 문제 해결하기
    Signed-off-by: ydm2790 <ydm2790@gmail.com>

--- src/main/java/com/sparta/cucumber/repository/CommentRepository.java
+++ src/main/java/com/sparta/cucumber/repository/CommentRepository.java
@@ -2,11 +2,15 @@ package com.sparta.cucumber.repository;
 
 import com.sparta.cucumber.models.Article;
 import com.sparta.cucumber.models.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @EntityGraph(attributePaths = {"article", "user"})
     List<Comment> findAllByArticle_Id(Long articleId);
+
+    @EntityGraph(attributePaths = {"article", "user"})
     List<Comment> findAllByArticle(Article article);
 }
