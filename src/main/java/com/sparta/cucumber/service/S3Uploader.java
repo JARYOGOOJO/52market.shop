05d2673542fa68d8485e1292b8bcd17457bca8eb
ydm2790 <ydm2790@gmail.com>
2021-12-14 17:00:57 +0900

    EB->S3 업로드 오류....왜 안되는 것이냐

--- src/main/java/com/sparta/cucumber/service/S3Uploader.java
+++ src/main/java/com/sparta/cucumber/service/S3Uploader.java
@@ -1,13 +1,17 @@
 package com.sparta.cucumber.service;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 import com.sparta.cucumber.dto.UserRequestDto;
 import com.sparta.cucumber.models.Article;
 import com.sparta.cucumber.repository.ArticleRepository;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
@@ -31,21 +36,51 @@ public class S3Uploader {
 
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;
+    @Value("${cloud.aws.s3.bucket.url}")
 
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
-                .orElseThrow(() -> new IllegalArgumentException("파일 변환 에러 MultipartFile -> File convert fail"));
-        return upload(uploadFile, dirName);
+    private String defaultUrl;
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+        String originName = multipartFile.getOriginalFilename();
+        String url;
+        try {
+            if (originName == null) {
+                throw new AssertionError();
+            }
+            String extension = originName.substring(originName.lastIndexOf("."));
+            String saveFileName = getName() + extension;
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(multipartFile.getContentType());
+            objectMetadata.setContentLength(multipartFile.getBytes().length);
+
+            InputStream inputStream = multipartFile.getInputStream();
+            uploadOnS3Bucket(saveFileName, inputStream, objectMetadata);
+            url = defaultUrl + saveFileName;
+        } catch (StringIndexOutOfBoundsException e) {
+            url = null;
+            System.out.println(e.getMessage());
+        }
+        return url;
     }
 
-    private String upload(File uploadFile, String dirName) {
+    private void uploadOnS3Bucket(String fileName, InputStream stream, ObjectMetadata data) {
+        TransferManager transferManager = TransferManagerBuilder.standard().withS3Client(amazonS3Client).build();
+        PutObjectRequest request = new PutObjectRequest(bucket, fileName, stream, data)
+                .withCannedAcl(CannedAccessControlList.PublicRead);
+        Upload upload = transferManager.upload(request);
+
+        try {
+            upload.waitForCompletion();
+        } catch (AmazonClientException | InterruptedException amazonClientException) {
+            log.error(amazonClientException.getMessage());
+        }
+    }
+
+    private static String getName() {
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-        String newFormat = simpleDateFormat.format(date);
-        String extension = newFormat + "_" + uploadFile.getName();
-        String fileName = dirName + "/" + extension;   // S3에 저장된 파일 이름
-
-        return putS3(uploadFile, fileName);
+        return simpleDateFormat.format(date);
     }
 
     // S3로 업로드
@@ -93,6 +128,5 @@ public class S3Uploader {
         );
         DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucket, "Article/" + article.getImageName());
         amazonS3Client.deleteObject(deleteObjectRequest);
-
     }
 }
