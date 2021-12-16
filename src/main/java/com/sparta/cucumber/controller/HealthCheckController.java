7d10ad75afd95e13de880d40ef7fd7b2383f1ecd
ydm2790 <ydm2790@gmail.com>
2021-12-16 19:55:09 +0900

    server time checker

--- src/main/java/com/sparta/cucumber/controller/HealthCheckController.java
+++ src/main/java/com/sparta/cucumber/controller/HealthCheckController.java
@@ -1,6 +1,7 @@
 package com.sparta.cucumber.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.joda.time.LocalDateTime;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,7 @@ public class HealthCheckController {
     @Operation(description = "서버 상태 체크", method = "GET")
     @GetMapping("/")
     public ResponseEntity<String> healthCheck() {
-        String result = "Hello World!!";
+        String result = LocalDateTime.now().toString();
         return ResponseEntity.ok(result);
     }
 }
