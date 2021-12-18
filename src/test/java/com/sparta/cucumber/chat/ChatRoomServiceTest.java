9a567b9b2f90bfad2c0cb2bd75e03b3112d81c87
ydm2790 <ydm2790@gmail.com>
2021-12-19 01:29:20 +0900

    message :: test files

---
+++ src/test/java/com/sparta/cucumber/chat/ChatRoomServiceTest.java
@@ -0,0 +1,28 @@
+package com.sparta.cucumber.chat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ChatRoomServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createRoom() {
+    }
+
+    @Test
+    void enterRoom() {
+    }
+
+    @Test
+    void exitRoom() {
+    }
+}
