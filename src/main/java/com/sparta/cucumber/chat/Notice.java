46d8f5701b9a52ba2a07faeebc2177df5d555940
ydm2790 <ydm2790@gmail.com>
2021-12-21 10:13:46 +0900

    message :: notice service

--- src/main/java/com/sparta/cucumber/chat/Notice.java
+++ src/main/java/com/sparta/cucumber/chat/Notice.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity(name = "message")
--- src/main/java/com/sparta/cucumber/chat/NoticeService.java
+++ src/main/java/com/sparta/cucumber/chat/NoticeService.java
@@ -1,11 +1,14 @@
 package com.sparta.cucumber.chat;
 
+import com.sparta.cucumber.error.CustomException;
 import com.sparta.cucumber.models.User;
 import com.sparta.cucumber.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.sparta.cucumber.error.ErrorCode.CHATROOM_NOT_FOUND;
+import static com.sparta.cucumber.error.ErrorCode.USER_NOT_FOUND;
 import static org.springframework.web.util.HtmlUtils.htmlEscape;
 
 @Service
@@ -20,10 +23,10 @@ public class NoticeService {
     public Notice message(ChatRequestDto requestDto) {
         User sender = userRepository
                 .findById(requestDto.getUserId()).orElseThrow(
-                        () -> new NullPointerException("잘못된 유저입니다."));
+                        () -> new CustomException(USER_NOT_FOUND));
         ChatRoom room = chatRoomRepository
                 .findById(requestDto.getRoomSubscribeId()).orElseThrow(
-                        () -> new NullPointerException("잘못된 대화방입니다."));
+                        () -> new CustomException(CHATROOM_NOT_FOUND));
         Notice message = Notice
                 .builder()
                 .senderId(sender.getId())
