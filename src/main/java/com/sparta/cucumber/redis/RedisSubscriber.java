41dc9e125073d780f6d73a729ef09e50008afcad
jihye0705 <jihyem75@gmail.com>
2021-12-21 22:20:52 +0900

    commit

--- src/main/java/com/sparta/cucumber/redis/RedisSubscriber.java
+++ src/main/java/com/sparta/cucumber/redis/RedisSubscriber.java
@@ -10,6 +10,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.web.util.HtmlUtils.htmlEscape;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -18,12 +20,32 @@ public class RedisSubscriber implements MessageListener {
     private final StringRedisTemplate redisTemplate;
     private final SimpMessageSendingOperations messagingTemplate;
 
+    // redis에 누군가 메시지를 전송했을시 구독자가 전달받는 부분
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
             String publishMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
             ChatRequestDto chatRequestDto = objectMapper.readValue(publishMessage, ChatRequestDto.class);
             log.debug("RedisSubscriber : " + chatRequestDto.toString());
+
+            switch (chatRequestDto.getMsgType()) {
+                case "chat":
+                    messagingTemplate.convertAndSend("/sub/chat/room/" + chatRequestDto.getRoomSubscribeId(), htmlEscape(chatRequestDto.getMsg()));
+                    break;
+                case "userNotice":
+                    messagingTemplate.convertAndSend("/sub/user/notice/" + chatRequestDto.getUserSubscribeId(), htmlEscape(chatRequestDto.getMsg()));
+                    break;
+                case "messageAll":
+                    messagingTemplate.convertAndSend("/sub/chat/all", htmlEscape(chatRequestDto.getMsg()));
+                    break;
+                case "articleNotice":
+                    messagingTemplate.convertAndSend("/sub/article/notice/all", htmlEscape(chatRequestDto.getMsg()));
+                    break;
+                case "commentNotice":
+                    messagingTemplate.convertAndSend("/sub/comment/notice/all", htmlEscape(chatRequestDto.getMsg()));
+                    break;
+            }
+
         } catch (Exception e) {
             log.error(e.getMessage());
         }
